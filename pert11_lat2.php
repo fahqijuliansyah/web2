1f5006f099a4064011665187ff02555ea708a001
Fahqi Juliansyah <80064168+fahqijuliansyah@users.noreply.github.com>
2021-03-26 23:48:53 +0700

    Delete pert11_lat2.php

--- pert11_lat2.php
+++
@@ -1,9 +0,0 @@
-<? 
-mysqli_connect("localhost","root",""); 
-$dbname="lat_dbase"; 
-$cek=mysqli_query("CREATE DATABASE $dbname") or die("Couldn't Create Database: 
-$dbname"); 
-if($cek){ 
- echo "Database $dbname berhasil dibuat"; 
-} 
-?>
